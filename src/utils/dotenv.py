fec06156f09de525995155da644df77db3604c90
victorleaoo <victor.pessoal1203@gmail.com>
2024-07-22 11:28:58 -0300

    correcao para deploy vercel

--- src/utils/dotenv.py
+++ src/utils/dotenv.py
@@ -1,5 +1,5 @@
 import os
-from constants import errorMessages
+from src.constants import errorMessages
 
 def validate_dotenv():
   required_env_var = ["SECRET", "ALGORITHM", "MAIL_USERNAME", "MAIL_PASSWORD", "MAIL_FROM", "MAIL_PORT", "MAIL_SERVER"]
